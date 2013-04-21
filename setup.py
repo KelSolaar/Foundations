aee2da59cc925e5777be4afabca6b40dae1b5a39
Thomas Mansencal <thomas.mansencal@gmail.com>
2013-04-21 08:23:19 +0200

    Update "Pypi" setup file.

--- setup.py
+++ setup.py
@@ -1,9 +1,66 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+**setup.py**
+
+**Platform:**
+	Windows, Linux, Mac Os X.
+
+**Description:**
+	`https://pypi.python.org/pypi/Foundations <https://pypi.python.org/pypi/Foundations>`_ package setup file.
+
+**Others:**
+
+"""
+
+#**********************************************************************************************************************
+#***	Future imports.
+#**********************************************************************************************************************
+from __future__ import unicode_literals
+
+#**********************************************************************************************************************
+#***	Encoding manipulations.
+#**********************************************************************************************************************
+import sys
+
+def _setEncoding():
+	"""
+	This definition sets the Application encoding.
+	"""
+
+	reload(sys)
+	sys.setdefaultencoding("utf-8")
+
+_setEncoding()
+
+#**********************************************************************************************************************
+#***	External imports.
+#**********************************************************************************************************************
 import re
 from setuptools import setup
 from setuptools import find_packages
 
+#**********************************************************************************************************************
+#***	Internal imports.
+#**********************************************************************************************************************
 import foundations.globals.constants
 
+#**********************************************************************************************************************
+#***	Module attributes.
+#**********************************************************************************************************************
+__author__ = "Thomas Mansencal"
+__copyright__ = "Copyright (C) 2008 - 2013 - Thomas Mansencal"
+__license__ = "GPL V3.0 - http://www.gnu.org/licenses/"
+__maintainer__ = "Thomas Mansencal"
+__email__ = "thomas.mansencal@gmail.com"
+__status__ = "Production"
+
+__all__ = ["getLongDescription"]
+
+#**********************************************************************************************************************
+#***	Module classes and definitions.
+#**********************************************************************************************************************
 def getLongDescription():
 	"""
 	This definition returns the Package long description.
@@ -32,7 +89,7 @@ setup(name=foundations.globals.constants.Constants.applicationName,
 	scripts=[],
 	url="https://github.com/KelSolaar/Foundations",
 	license="GPLv3",
-	description="Foundations is the core package of Umbra, sIBL_GUI, sIBL_Reporter.",
+	description="Foundations is the core package of Umbra, sIBL_GUI and sIBL_Reporter.",
 	long_description=getLongDescription(),
 	install_requires=["ordereddict>=1.1", "sphinx>=1.1.3", "unittest2>=0.5.1"],
 	classifiers=["Development Status :: 5 - Production/Stable",
