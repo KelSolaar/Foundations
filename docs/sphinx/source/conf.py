c0a4101bc2dccdfbea73e57d6ef94d422019d1e1
Thomas Mansencal <thomas.mansencal@gmail.com>
2014-03-15 21:04:23 +0100

    Change copyrights dates in "docs.sphinx.source.conf" module.

--- docs/sphinx/source/conf.py
+++ docs/sphinx/source/conf.py
@@ -49,7 +49,7 @@ master_doc = "index"
 
 # General information about the project.
 project = u"Foundations"
-copyright = u"2012 - 2014, Thomas Mansencal"
+copyright = u"2008 - 2014, Thomas Mansencal"
 
 # The version info for the project you"re documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
